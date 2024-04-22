765d06a76be523f46c6f7f93be37b9780e98ab49
Farhin-Rashid <123594031+Farhin-Rashid@users.noreply.github.com>
2024-04-21 19:27:25 -0500

    Added recyclerview

--- app/src/main/java/com/example/anime_studio_app/MainActivity.kt
+++ app/src/main/java/com/example/anime_studio_app/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.RequestParams
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
@@ -13,10 +15,17 @@ import com.codepath.asynchttpclient.callback.TextHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
+    //private val title = mutableListOf<String>()
+    //private val epCount = mutableListOf<String>()
+    //private val poster = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
+        //recyclerView.layoutManager = LinearLayoutManager(this)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
